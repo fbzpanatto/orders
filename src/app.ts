4c2c76a7ba0788c13c17e66abc046dbdb33704a6
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-02 09:54:44 -0300

    Staging

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+// require('express-async-errors');
 import 'express-async-errors';
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
@@ -16,14 +17,19 @@ app.use(helmet())
 
 app.use(express.json())
 
-app.use('/api', (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World")
-})
+app.use('/api', (req: Request, res: Response) => { res.status(200).send("Hello World") })
+
+// app.use('/error', async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     throw new Error('teste2 deu erro');
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// })
 
 app.use('/customers/', customerRouter);
 
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  res.status(500).send(error.message)
-})
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => { res.status(500).send(error.message) })
 
 export default app
