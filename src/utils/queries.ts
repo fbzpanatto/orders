db8d091daa69a6a682c73224d9a85923f8018490
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-07 14:33:45 -0300

    Staging

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -6,19 +6,6 @@ export const findOneRegister = async (table: string, field: string, value: strin
   return await query(`SELECT * FROM ${table} WHERE ${field}=${value}`)
 }
 
-export const updateRow = async (table: string, whereField: string, param: number, body: { [key: string]: any }, bodyFieldsToIgnore: string[]) => {
-
-  const rowUpdates = Object.entries(body)
-    .filter(([key]) => !bodyFieldsToIgnore.includes(key))
-    .map(([key, value]) => typeof value === 'number' ? `${key}=${value}` : `${key}='${value}'`)
-
-  const queryString = `UPDATE ${table} SET ${rowUpdates.join(', ')} WHERE ${whereField}=${param}`;
-
-  const queryResult = await query(queryString) as ResultSetHeader;
-
-  return setResponse(200, 'Registro atualizado com sucesso.', queryResult.affectedRows)
-};
-
 export const createRow = async (table: string, body: { [key: string]: any }, bodyFieldsToIgnore: string[]) => {
 
   let keys: string[] = []
@@ -37,4 +24,17 @@ export const createRow = async (table: string, body: { [key: string]: any }, bod
   const queryResult = await query(queryString) as ResultSetHeader;
 
   return setResponse(200, 'Registro criado com sucesso.', queryResult.affectedRows)
+};
+
+export const updateRow = async (table: string, whereField: string, param: number, body: { [key: string]: any }, bodyFieldsToIgnore: string[]) => {
+
+  const rowUpdates = Object.entries(body)
+    .filter(([key]) => !bodyFieldsToIgnore.includes(key))
+    .map(([key, value]) => typeof value === 'number' ? `${key}=${value}` : `${key}='${value}'`)
+
+  const queryString = `UPDATE ${table} SET ${rowUpdates.join(', ')} WHERE ${whereField}=${param}`;
+
+  const queryResult = await query(queryString) as ResultSetHeader;
+
+  return setResponse(200, 'Registro atualizado com sucesso.', queryResult.affectedRows)
 };
