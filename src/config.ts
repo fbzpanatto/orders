b1c4c57f72e025e8edd8f23dc042add6bc8eb8f3
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-08 12:37:53 -0300

    Staging

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   db: {
-    host: "localhost",
-    user: "fbzpanatto",
-    password: "fnp181292",
-    database: "mydb",
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
     connectTimeout: 60000
   },
   listPerPage: 10,
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,18 @@
 import { describe, test, expect } from "@jest/globals"
 import { sum } from './index'
+import supertest from 'supertest'
+import app from '../app'
 
-describe('Sum function', () =>{
-  test('Returns correct value', () =>{
-      expect(sum(2, 3)).toEqual(5)
+const request = supertest(app)
+
+describe('Sum function2', () => {
+  it('Returns correct value', () => {
+    expect(sum(2, 3)).toEqual(5)
+  })
+
+  it('Return Hello World', () => {
+    const response = request.get('/api')
+
+    expect(response.ok).toBeTruthy()
   })
 })
