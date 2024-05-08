add8f0e5cc61c493dde85fcef7f426810d80b724
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-08 15:52:25 -0300

    Staging

--- src/services/db.ts
+++ src/services/db.ts
@@ -1,11 +1,30 @@
 import mysql from 'mysql2/promise'
 import { config } from '../config'
 
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'fbzpanatto',
+  database: 'mydbTest',
+  password: 'fnp181292',
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: config().db.connectTimeout, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+// export async function query(sql: string, params?: any) {
+
+//   const connection = await mysql.createConnection(config().db)
+
+//   const [results,] = await connection.execute(sql, params)
+
+//   return results
+// }
+
 export async function query(sql: string, params?: any) {
-
-  const connection = await mysql.createConnection(config().db)
-
-  const [results,] = await connection.execute(sql, params)
-
+  const [results,] = await pool.query(sql, params)
   return results
 }
--- src/tests/integration/index.test.ts
+++ src/tests/integration/index.test.ts
@@ -1,16 +1,17 @@
 import { describe, expect } from "@jest/globals"
-import dotenv from 'dotenv';
+import { query } from "../../services/db";
 import request from 'supertest'
 import app from '../../app'
-import { createConnection } from "../../services/db";
-
-dotenv.config();
 
 describe('Endpoints', () => {
 
-  // afterAll(async () => {
-  //   (await createConnection()).end()
-  // })
+  beforeAll(async () => {
+    await query('SET FOREIGN_KEY_CHECKS = 0')
+    await query('TRUNCATE TABLE legal_persons')
+    await query('TRUNCATE TABLE normal_persons')
+    await query('TRUNCATE TABLE persons')
+    await query('SET FOREIGN_KEY_CHECKS = 1')
+  })
 
   it('Return Hello World', async () => {
     const response = await request(app).get('/api')
@@ -18,21 +19,21 @@ describe('Endpoints', () => {
     expect(response.ok).toBeTruthy()
   })
 
-  it('Return Persons', async () => {
-    const response = await request(app).get('/persons')
+  // it('Return Persons', async () => {
+  //   const response = await request(app).get('/persons')
 
-    expect(response.body).toEqual({ "data": [], "message": "Consulta realizada com sucesso.", "meta": { "page": 1 }, "status": 200 })
-  })
+  //   expect(response.body).toEqual({ "data": [], "message": "Consulta realizada com sucesso.", "meta": { "page": 1 }, "status": 200 })
+  // })
 
   it('Create Normal Person', async () => {
 
     const response = await request(app).post('/persons').send({
       first_name: "João",
       last_name: "Ninguém",
-      cpf: "12345678900",
+      cpf: "36937725877",
       person_category_id: 2
     })
 
-    // expect(response.body).toEqual({})
+    expect(response.body).toEqual({ "message": "Registro criado com sucesso.", "status": 200 })
   })
 })
