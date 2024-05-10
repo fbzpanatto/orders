d161dc8400e91b870c067f0e8a78086df00a6370
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-10 12:08:56 -0300

    Staging

--- src/middlewares/validators.ts
+++ src/middlewares/validators.ts
@@ -9,24 +9,28 @@ import { phonesPATCH, phonesPOST } from '../schemas/phones'
 export const validateId = check('id').not().isEmpty().isNumeric()
 export const validatePersonId = check('personId').not().isEmpty().isNumeric()
 
+// Legal
 export const validatePatchLegal = checkSchema(legalPATCH);
 export const validatePatchNormal = checkSchema(normalPATCH);
 export const bodyValidationNormal = (req: Request, res: Response, next: NextFunction) => {
   return !validationResult(req).isEmpty() ? invalidValues(res) : unexpectedFieldsFn(req, res, next, normalPOST)
 };
 
+// Normal
 export const validatePostLegal = checkSchema(legalPOST);
 export const validatePostNormal = checkSchema(normalPOST);
 export const bodyValidationLegal = (req: Request, res: Response, next: NextFunction) => {
   return !validationResult(req).isEmpty() ? invalidValues(res) : unexpectedFieldsFn(req, res, next, legalPOST)
 };
 
+// Address
 export const validatePostAddresses = checkSchema(addressesPOST)
 export const validatePatchAddresses = checkSchema(addressesPATCH)
 export const bodyValidationAddress = (req: Request, res: Response, next: NextFunction) => {
   return !validationResult(req).isEmpty() ? invalidValues(res) : unexpectedFieldsFn(req, res, next, addressesPOST)
 };
 
+// Phone
 export const validatePostPhones = checkSchema(phonesPOST)
 export const validatePatchPhones = checkSchema(phonesPATCH)
 export const bodyValidationPhone = (req: Request, res: Response, next: NextFunction) => {
