4253d99307adb95d47c2e554d3ee251b9fb2e6e0
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-10 12:08:02 -0300

    Staging

--- src/middlewares/validators.ts
+++ src/middlewares/validators.ts
@@ -33,10 +33,7 @@ export const bodyValidationPhone = (req: Request, res: Response, next: NextFunct
   return !validationResult(req).isEmpty() ? invalidValues(res) : unexpectedFieldsFn(req, res, next, phonesPOST)
 };
 
-const invalidValues = (res: Response) => {
-  return res.status(400).json(objectResponse(400, 'Valor(es) inválido(s) no corpo da requisição.'))
-}
-
+const invalidValues = (res: Response) => { return res.status(400).json(objectResponse(400, 'Valor(es) inválido(s) no corpo da requisição.')) }
 const unexpectedFieldsFn = (req: Request, res: Response, next: NextFunction, schema: { [key: string]: any }) => {
   const unexpectedFields = Object.keys(req.body).filter(key => !schema.hasOwnProperty(key));
   return unexpectedFields.length ? res.status(400).json(objectResponse(400, 'Campo(s) inesperado(s) no corpo da requisição.')) : next()
