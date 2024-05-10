ad06ce43c90b23dd992191368f843f6730fe7cec
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-10 12:13:13 -0300

    Staging

--- src/middlewares/validators.ts
+++ src/middlewares/validators.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import { check, checkSchema, validationResult } from 'express-validator'
-import { objectResponse } from '../utils/response'
+import { unexpectedFieldsFn, invalidValues } from '../utils/bodyValidations'
+
 import { legalPATCH, legalPOST } from '../schemas/legalPerson'
 import { normalPATCH, normalPOST } from '../schemas/normalPerson'
 import { addressesPATCH, addressesPOST } from '../schemas/addresses'
@@ -36,9 +37,3 @@ export const validatePatchPhones = checkSchema(phonesPATCH)
 export const bodyValidationPhone = (req: Request, res: Response, next: NextFunction) => {
   return !validationResult(req).isEmpty() ? invalidValues(res) : unexpectedFieldsFn(req, res, next, phonesPOST)
 };
-
-const invalidValues = (res: Response) => { return res.status(400).json(objectResponse(400, 'Valor(es) inválido(s) no corpo da requisição.')) }
-const unexpectedFieldsFn = (req: Request, res: Response, next: NextFunction, schema: { [key: string]: any }) => {
-  const unexpectedFields = Object.keys(req.body).filter(key => !schema.hasOwnProperty(key));
-  return unexpectedFields.length ? res.status(400).json(objectResponse(400, 'Campo(s) inesperado(s) no corpo da requisição.')) : next()
-}
---
+++ src/utils/bodyValidations.ts
@@ -0,0 +1,9 @@
+import { objectResponse } from '../utils/response'
+import { Request, Response, NextFunction } from 'express'
+
+export const invalidValues = (res: Response) => { return res.status(400).json(objectResponse(400, 'Valor(es) inválido(s) no corpo da requisição.')) }
+
+export const unexpectedFieldsFn = (req: Request, res: Response, next: NextFunction, schema: { [key: string]: any }) => {
+  const unexpectedFields = Object.keys(req.body).filter(key => !schema.hasOwnProperty(key));
+  return unexpectedFields.length ? res.status(400).json(objectResponse(400, 'Campo(s) inesperado(s) no corpo da requisição.')) : next()
+}
