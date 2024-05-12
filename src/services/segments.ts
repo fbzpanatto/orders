834deea33dd635af49187015365b633b1dd6a724
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-12 15:07:32 -0300

    Staging

--- src/services/segments.ts
+++ src/services/segments.ts
@@ -1,10 +1,17 @@
 import { objectResponse } from '../utils/response';
 import { Segments } from '../interfaces/segments';
-import { selectAllFromWhere, updateTableSetWhere, insertInto } from '../utils/queries';
+import { selectAllFromWhere, updateTableSetWhere, insertInto, selectAllFrom } from '../utils/queries';
 import { DatabaseTables } from '../enums/tables';
 import { Request } from 'express';
 
-export const getSegments = async (segmentId: number) => {
+export const getSegments = async () => {
+  try {
+    const rows = await selectAllFrom<Segments>(DatabaseTables.segments)
+    return objectResponse(200, 'Consulta realizada com sucesso.', { data: rows })
+  } catch (error) { return objectResponse(400, 'Não foi possível processar a sua solicitação.') }
+}
+
+export const getSegment = async (segmentId: number) => {
   try {
     const result = await selectAllFromWhere(DatabaseTables.segments, 'person_id', segmentId) as Array<Segments>
     return objectResponse(200, 'Consulta realizada com sucesso.', { result })
--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -1,8 +1,11 @@
 import { ResultSetHeader, format } from 'mysql2';
 import { query } from '../services/db'
 
-export const selectAllFrom = async () => {
+export async function selectAllFrom<T>(table: string) {
 
+  const queryString = `SELECT * FROM ${table}`
+
+  return await query(format(queryString)) as Array<T>
 }
 
 export const selectAllFromWhere = async (table: string, column: string, columnValue: string | number) => {
@@ -16,25 +19,25 @@ export const selectAllFromWhere = async (table: string, column: string, columnVa
   return await query(format(queryString, values)) as Array<{ [key: string]: any }>
 };
 
-export const insertInto = async (table: string, body: { [key: string]: any }, bodyFieldsToIgnore: string[]) => {
+export const insertInto = async (table: string, body: { [key: string]: any }, fieldsToIgnore: string[]) => {
 
   const columns = Object.entries(body)
-    .filter(([key]) => !bodyFieldsToIgnore.includes(key))
+    .filter(([key]) => !fieldsToIgnore.includes(key))
     .map(([key]) => key);
 
   const placeholders = columns.map(() => '?')
 
   const queryString = `INSERT INTO ${table} (${columns.join(', ')}) VALUES (${placeholders.join(', ')})`;
 
-  const values = Object.values(body).filter((_, index) => !bodyFieldsToIgnore.includes(columns[index]));
+  const values = Object.values(body).filter((_, index) => !fieldsToIgnore.includes(columns[index]));
 
   return await query(format(queryString, values)) as ResultSetHeader
 };
 
-export const updateTableSetWhere = async (table: string, column: string, columnValue: number, body: { [key: string]: any }, bodyFieldsToIgnore: string[]) => {
+export const updateTableSetWhere = async (table: string, column: string, columnValue: number, body: { [key: string]: any }, fieldsToIgnore: string[]) => {
 
   const includedColumns = Object.entries(body)
-    .filter(([key]) => !bodyFieldsToIgnore.includes(key))
+    .filter(([key]) => !fieldsToIgnore.includes(key))
     .map(([key, value]) => ({ key, value }));
 
   const queryString = `UPDATE ${table} SET ${includedColumns.map(({ key }) => `${key}=?`).join(', ')} WHERE ${column}=?`;
