5b82979a794ac04ef74af5e6f3308743f61a0337
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-14 19:15:51 -0300

    Staging

--- src/interfaces/order_products.ts
+++
@@ -1,5 +0,0 @@
-export interface OrderProducts {
-  id?: number,
-  order_id?: number,
-  product_id?: number
-}
--- src/interfaces/order_products_status.ts
+++ src/interfaces/order_products_status.ts
@@ -1,6 +1,7 @@
 export interface OrderProductsStatus {
   id?: number,
-  order_products_id?: number,
+  order_id?: number,
+  product_id?: number,
   status_id?: number,
   started_at?: Date,
   ended_at?: Date
