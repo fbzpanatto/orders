417d1f78d580241f6dbfd3286a72f821b4a7ebc1
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-05-18 12:46:59 -0300

    Status

--- src/middlewares/validators.ts
+++ src/middlewares/validators.ts
@@ -79,7 +79,7 @@ export const bodyValidationOrders = (req: Request, res: Response, next: NextFunc
   return !validationResult(req).isEmpty() ? invalidValues(res, req) : unexpectedFieldsFn(req, res, next, ordersPOST)
 };
 
-// Orders
+// OrderProductStatus
 export const validatePostOrderProductStatus = checkSchema(ordersProductStatusPOST)
 export const validatePatchProductStatus = checkSchema(ordersProductStatusPATCH)
 export const bodyValidationProductStatus = (req: Request, res: Response, next: NextFunction) => {
