1e3dd6cb5149afe0e616587c4a8679890d238397
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-06-18 14:02:56 -0300

    Staging

--- src/routes/fields.ts
+++ src/routes/fields.ts
@@ -5,6 +5,11 @@ import { User } from '../interfaces/users'
 
 const router = Router()
 
+// router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+//   const result = await getCompanies(1)
+//   return res.status(result.status).json(result)
+// })
+
 router.patch('/:id', validateId, validatePatchField, bodyValidationField, async (req: Request, res: Response, next: NextFunction) => {
   const result = await updateUser(parseInt(req.params.id), req.body as User)
   return res.status(result.status).json(result)
