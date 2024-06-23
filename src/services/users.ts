18f53371b7a2f10ccf55eb6143e8ead55fd37533
Fabrizio Panato <fbzpanatto@hotmail.com>
2024-06-23 12:48:11 -0300

    Staging

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,60 +1,67 @@
 import { objectResponse } from '../utils/response';
-import { updateTableSetWhere, insertInto, selectAllFrom, selectMaxColumn } from '../utils/queries';
+import { updateTableSetWhere, insertInto, selectMaxColumn, selectWithJoinsAndWhere } from '../utils/queries';
 import { Tables } from '../enums/tables';
 import { emptyOrRows } from '../helper';
 import { dbConn } from './db';
 import { PoolConnection } from 'mysql2/promise';
-import { format, ResultSetHeader } from 'mysql2';
+import { ResultSetHeader } from 'mysql2';
 import { Request } from 'express'
 import { User } from '../interfaces/users';
 
-const company_id = 'company_id'
 const USER_ID = 'user_id'
 const COMPANY_ID = 'company_id'
 const MAX_USER_ID = 'max_user_id'
 
 export const getUsers = async (request: Request, page: number) => {
 
-  let connection = null;
+  const { company_id, user_id } = request.query
+
+  let conn = null;
+
+  const baseTable = 'users';
+  const baseAlias = 'u';
 
   try {
 
-    connection = await dbConn()
+    conn = await dbConn()
 
-    const queryString = `
-    SELECT u.user_id, u.name, u.active, u.username, u.created_at, c.corporate_name, r.role_name
-    FROM ${Tables.users} AS u
-    LEFT JOIN ${Tables.companies} AS c ON u.${company_id} = c.${company_id}
-    LEFT JOIN ${Tables.roles} AS r ON u.${company_id} = r.${USER_ID}
-    `
+    if (company_id && user_id) {
 
-    const rows = await selectAllFrom<User>(connection, Tables.users, page, queryString)
-    const data = emptyOrRows(rows);
+    }
+
+    const selectFields = [
+      'u.name', 'u.username', 'u.active', 'u.created_at', 'r.role_id, r.role_name', 'c.corporate_name'
+    ];
+    const whereConditions = {}
+    const joins = [
+      {
+        table: 'roles',
+        alias: 'r',
+        conditions: [
+          { column1: 'u.company_id', column2: 'r.company_id' },
+          { column1: 'u.role_id', column2: 'r.role_id' }
+        ]
+      },
+      {
+        table: 'companies',
+        alias: 'c',
+        conditions: [
+          { column1: 'c.company_id', column2: 'r.company_id' }
+        ]
+      }
+    ]
+
+    const result = await selectWithJoinsAndWhere(
+      conn, baseTable, baseAlias, selectFields, whereConditions, joins
+    )
+
+    const data = emptyOrRows(result);
     const meta = { page };
 
     return objectResponse(200, 'Consulta realizada com sucesso.', { data, meta })
   }
   catch (error) { return objectResponse(400, 'Não foi possível processar sua solicitação.', {}) }
-  finally { if (connection) { connection.release() } }
-}
-
-export const getUserById = async (userId: number) => {
-
-  let connection = null;
-
-  try {
-
-    connection = await dbConn()
-
-    const queryString = `SELECT * FROM ${Tables.users} AS u WHERE u.user_id=?`
-
-    const [result] = await connection.query(format(queryString, [userId]))
-    const data = (result as Array<User>)[0]
-
-    return objectResponse(200, 'Consulta realizada com sucesso.', { data })
-  }
-  catch (error) { return objectResponse(400, 'Não foi possível processar a sua solicitação.') }
-  finally { if (connection) { connection.release() } }
+  finally { if (conn) { conn.release() } }
 }
 
 export const createUser = async (body: User) => {
@@ -66,8 +73,13 @@ export const createUser = async (body: User) => {
     conn = await dbConn()
     await conn.beginTransaction()
 
-    const newUserId = await selectMaxColumn(conn, Tables.users, USER_ID, MAX_USER_ID, COMPANY_ID, (body.company_id as number))
-    const [queryResult] = await insertInto(conn, Tables.users, { ...body, user_id: newUserId }, [])
+    const newUserId = await selectMaxColumn(
+      conn, Tables.users, USER_ID, MAX_USER_ID, COMPANY_ID, (body.company_id as number)
+    )
+
+    const [queryResult] = await insertInto(
+      conn, Tables.users, { ...body, user_id: newUserId }, []
+    )
 
     const affectedRows = (queryResult as ResultSetHeader).affectedRows
 
@@ -75,10 +87,7 @@ export const createUser = async (body: User) => {
 
     return objectResponse(200, 'Registro criado com sucesso.', { affectedRows });
   }
-  catch (error) {
-    console.log(error)
-    return rollBackCatchBlock(error, conn)
-  }
+  catch (error) { return rollBackCatchBlock(error, conn) }
   finally { if (conn) { conn.release() } }
 }
 
